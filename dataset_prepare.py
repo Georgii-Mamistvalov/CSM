2d8816a18103e117fc91afb5af677bf555ad3a70
Georgii-Mamistvalov <76109443+Georgii-Mamistvalov@users.noreply.github.com>
2022-07-05 20:15:17 +0300

    Update dataset_prepare.py

--- dataset_prepare.py
+++ dataset_prepare.py
@@ -1,60 +1,100 @@
-# Цель данного блока - получить набор pdb-структур из датасета (в фомате .csv). Также мы переводим изначальный датасет в общий формат (единые столбцы).
-
 import csv
-import os
 import pandas as pd
 import numpy as np
-from Bio.PDB import PDBParser # для выделения нужной цепи и нужной модели
-from Bio.PDB import PDBIO # для сохранения результата
-from Bio.PDB import Select # для выбора только нужной цепи
-from pdbtools import pdb_fetch # для скачивания PDB
+import glob
+from Bio.PDB import PDBParser, PDBIO, Select # for needed model and chain
+from pdbtools import pdb_fetch # for raw PDB download
+import subprocess # for run bash script
+from schrodinger import structure # for PRR
+from schrodinger.protein import reliability # for PRR
 
-# Загружаем датасет из CSV в pd.DataFrame 
+# Upload dataset from CSV to pd.DataFrame 
 
 PDB_dataset = pd.read_csv("/path/to/datadrame.csv")
 
-'''Предполагается наличие столбцов типа 
+'''We should have such columns 
 'PDB' (PDB_id; str), 
-'WILD_TYPE' (нативный остаток однобуквенный; str), 
-'CHAIN' (цепь, в которой выполняется замена; str),
-'POSITION' (номер заменяемого остатка; str), 
-'MUTANT' (мутантный остаток однобуквенный; str), 
-'DDG' (экспериментально полученное ddG, с ним будем сравнивать и на нем будем учить модель; float)
-Если столбцы не такие - стоит удалить ненужные и переименовать нужные в указанные выше'''
+'WILD_TYPE' (naive one-letter residue; str), 
+'CHAIN' (chain, where mutation occurs; str),
+'POSITION' (mutation residue number, could be with letters; str), 
+'MUTANT' (mutant one-letter residue; str), 
+'DDG' (experimental ddG; float)
+If columns are different - delete unneeded and rename left as above'''
 
-# Создаем доп. столбец PDB+CHAIN, понадобится для этапа (# Обрабатываем, сохраняя только первую модель и нужную цепь)
+# Drop duplicates and save only mean value of ddG for one mutation (if there are values with different ddG sign for one mutation - delete it)
 
-PDB_dataset["PDB_id_chain"] = PDB_dataset["PDB"] + PDB_dataset["CHAIN']
+PDB_dataset = PDB_dataset.drop_duplicates()
+PDB_dataset = PDB_dataset.groupby(['PDB', 'CHAIN', 'WILD_TYPE', 'POSITION', 'MUTANT'], as_index=False).filter(lambda x:(x.ddG.all() > 0) || (x.ddG.all() < 0))
+PDB_dataset = PDB_dataset.groupby(['PDB', 'CHAIN', 'WILD_TYPE', 'POSITION', 'MUTANT'], as_index=False).aggregate({"ddG":"mean"})
 
-# Получаем список уникальных PDB_id из датасета 
 
-PDB_unique = PDB_dataset.PDB.unique()
 
-# Скачиваем все уникальные PDB в первоначальном виде
+# Create extra column PDB_CHAIN, will need it later
+
+PDB_dataset["PDB_CHAIN"] = PDB_dataset["PDB"] + PDB_dataset["CHAIN']
+
+# Get unique PDB_id from dataset 
+
+PDB_unique = PDB_dataset["PDB"].unique()
+
+# Download raw PDB 
 
 for PDB_id in PDB_unique:
-    PDB_file = pdb_fetch.run(PDB_id[0:4])
-    with open("/path/to/folder/"+PDB_id[0:4] + '.pdb',mode="a") as f:
+    PDB_file = pdb_fetch.run(PDB_id[:4])
+    with open(f"/path/to/folder/{PDB_id[:4]}.pdb",mode="a") as f:
         for i in PDB_file:
             f.write(i)
             
-# Получаем список уникальных PDB_id_chain из датасета 
+# Get unique PDB_CHAIN from dataset 
 
-PDB_id_chain_unique = PDB_dataset.PDB_id_chain.unique()
+PDB_id_chain_unique = PDB_dataset['PDB_CHAIN'].unique()
 
                                                                
-# Делаем класс для удаления гетероатомов и всего, кроме белка (ВРОДЕ ТАК, НАДО ЭТО УТОЧНИТЬ)  
+# Create class to remove heteroatoms and non-protein atoms   
                                                                
 class NonHetSelect(Select):
     def accept_residue(self, residue):
         return 1 if residue.id[0] == " " else 0                         
                                                                
-# Обрабатываем, сохраняя только первую модель и нужную цепь.
+# Save only 1 model and needed chain (or chains if several for one protein)
 
 parser = PDBParser()
 io = PDBIO()
 for PDB_id_chain in PDB_id_chain_unique:    
-    structure = parser.get_structure(PDB_id_chain[0:4], "/path/to/folder/"+PDB_id_chain[0:4] + '.pdb')
-    chain = structure[0][PDB_id_chain[4]] # берем первую модель и нужную цепь
+    structure = parser.get_structure(PDB_id_chain[:4], f"/path/to/folder/{PDB_id_chain[:4]}.pdb")
+    chain = structure[0][PDB_id_chain[4]] # get 1 model and needed chain
     io.set_structure(chain)
-    io.save("/path/to/folder/"+PDB_id_chain[0:5]+"_model_1.pdb", NonHetSelect())
+    io.save(f"/path/to/folder/{PDB_id_chain[:5]}_model_1.pdb", NonHetSelect())
+
+# Apply Protein Preparation Wizard (PPW) to all structures (can be done only in bash). Make sure you can run Schrodinger from command line
+# As a result we get {PDB_CHAIN}_prep.pdb files
+                                                               
+import subprocess
+subprocess.run('dataset_prepwiz.sh')
+
+# Check structures after PPW by Protein Reliability Report (PRR) for missing atoms, missung chains and steric clashes
+
+errors = {}
+for pdb_file in glob.glob("*_prep.pdb"):
+    st = structure.StructureReader.read(pdb_file)
+    check = reliability.run_quick_check(st)
+    if check.getTextSummary() != []:
+        errors[pdb_file] = check.getTextSummary()
+structures_with_errors = []
+for structure, report in errors.items():
+    problems = ['missing loops','missing atoms','steric clashes']
+    matching = [report_item for report_item in report if any(problem in report_item for problem in problems)]
+    if matching != []:
+        print("There is a problem with this structure:", structure)
+        structures_with_errors.append(structure[:5])
+
+# Delete all structures from previous step from dataset
+                                                            
+for structure in structures_with_errors:
+    PDB_dataset.drop(PDB_dataset[PDB_dataset['PDB_CHAIN'] == structure].index, inplace=True)
+
+# Save filtered dataset
+                                                            
+PDB_dataset.to_csv(f'/path/to/folder/dataset.csv', header=True, index=False, mode='w')
+
+# As a result we get dataset.csv file with needed columns and bunch of {PDB_CHAIN}_prep.pdb files
