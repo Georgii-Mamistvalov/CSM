a2647de4bb796c9e4d665902622ac7275b185bf1
Georgii-Mamistvalov <76109443+Georgii-Mamistvalov@users.noreply.github.com>
2022-07-11 11:08:42 +0300

    Update get_RS.py

--- get_RS.py
+++ get_RS.py
@@ -2,8 +2,10 @@ import glob
 import dhutil
 import pandas as pd
 import csv
+import re
+import os
 from schrodinger import structure
-from Bio.SeqUtils import seq3
+from Bio.SeqUtils import seq3, seq1
 
 # .pdb to .mae
 
@@ -27,3 +29,30 @@ for PDB_id_chain in PDB_dataset.PDB_CHAIN.unique():
     command = '"${SCHRODINGER}/run" residue_scanning_backend.py -WAIT -jobname residue_scanning_CSM -fast -res_file ' + mutations_filename + " -refine_mut prime_residue -calc hydropathy,rotatable,vdw_surf_comp,sasa_polar,sasa_nonpolar,sasa_total -dist 0.00 " + residue_scanning_file + " -add_res_scan_wam -HOST localhost:1 -NJOBS 1 -TMPLAUNCHDIR"
     with open(f'/path/to/{PDB_id_chain[0:5]}/{PDB_id_chain[0:5]}.sh', "w") as text_file:
         print(command, file=text_file)
+
+# After running RS, put you result folders named PDB_CHAIN into RS_done folder. Now we add DDG_RS column to dataset with ddG values after RS
+
+os.chdir(f'/path/to/RS_done')
+PDB_dataset["DDG_RS"] = 0
+for pdb_file in glob.glob("*"):
+    print(pdb_file)    
+    os.chdir(f'/path/to/RS_done/{pdb_file}')
+    PDB_dataset_temp = PDB_dataset.loc[PDB_dataset["PDB_CHAIN"] == pdb_file]
+    for ind in number_of_mut:
+        st = structure.StructureReader.read('residue_scanning_CSM-out.maegz', index=ind+2)
+        mutation = st.property['s_bioluminate_Mutations']
+        mutation_split = re.search('(.):(.*)\((.*)->(.*)\)', mutation)
+        chain = mutation_split.group(1)
+        position = mutation_split.group(2)
+        wt = seq1(mutation_split.group(3), undef_code='H')
+        mut = seq1(mutation_split.group(4), undef_code='H')
+        index = PDB_dataset.loc[(PDB_dataset["CHAIN"] == chain) &
+                        (PDB_dataset["POSITION"] == position) &
+                        (PDB_dataset["WILD_TYPE"] == wt) &
+                        (PDB_dataset["MUTANT"] == mut)].index
+        PDB_dataset.at[index,"DDG_RS"] = -st.property['r_psp_Prime_delta_Stability']
+    os.chdir(f'/Users/georgii_mamistvalov/Documents/Projects/CSM/Datasets_stability/S2648_after_prepwizard_done')
+
+# Save updated dataset
+
+PDB_dataset.to_csv(f'/path/to/dataset.csv', header=True, index=False, mode='w')
